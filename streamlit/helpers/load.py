cdc04332493a80bce419c7bceeb4dd1b5f7f9e47
ajay katte <ajaymkatteo.0@gmail.com>
2026-04-19 18:17:41 +0000

    fallback to github csv file

--- streamlit/helpers/load.py
+++ streamlit/helpers/load.py
@@ -29,5 +29,5 @@ def fetch_table(table_name, csv_path):
             df = pd.read_csv(f'/opt/airflow/streamlit/{csv_path}')
         except:
             file_path = f'https://raw.githubusercontent.com/mushroomsandchai/osm/refs/heads/main/streamlit/{csv_path}'
-            df = pd.read_csv(f'/workspaces/osm/streamlit/{csv_path}')
+            df = pd.read_csv(file_path)
         return df, "CSV"
