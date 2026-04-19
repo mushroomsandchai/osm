d4300f24ef29b8ef7894d8cc0e282b96e9e4d93b
mushroomsandchai <ajaymkatteo.0@gmail.com>
2026-04-19 18:03:02 +0000

    trying to get the streamlit app cloud compatible

--- streamlit/helpers/load.py
+++ streamlit/helpers/load.py
@@ -1,22 +1,23 @@
-import os
-import uuid
 import pandas as pd
 import streamlit as st
-from google.cloud import bigquery
-
 
 @st.cache_data(ttl = 60 * 60 * 24 * 30)
 def fetch_table(table_name, csv_path):
     """
-    Try to fetch table from BigQuery, fallback to CSV if it fails.
-    Returns: (dataframe, source)
+        Try to fetch table from BigQuery, fallback to CSV if it fails.
+        Returns: (dataframe, source)
     """
-    client = bigquery.Client()
-    job_id = f"streamlit_query_{uuid.uuid4().hex}"  # unique per call
-
-    project = os.environ.get('PROJECT')
-    dataset = os.environ.get('DATASET')
+    
     try:
+        import os
+        import uuid
+        from google.cloud import bigquery
+
+        client = bigquery.Client()
+        job_id = f"streamlit_query_{uuid.uuid4().hex}"  # unique per call
+
+        project = os.environ.get('PROJECT')
+        dataset = os.environ.get('DATASET')
         query_job = client.query(
             f"SELECT * FROM `{project}.{dataset}.{table_name}`",
             job_id=job_id
@@ -24,5 +25,8 @@ def fetch_table(table_name, csv_path):
         df = query_job.result().to_dataframe()
         return df, "BigQuery"
     except Exception:
-        df = pd.read_csv(f'/opt/airflow/streamlit/{csv_path}')
+        try:
+            df = pd.read_csv(f'/opt/airflow/streamlit/{csv_path}')
+        except:
+            df = pd.read_csv(csv_path)
         return df, "CSV"
